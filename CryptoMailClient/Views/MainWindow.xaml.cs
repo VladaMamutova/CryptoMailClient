d65c613ec0c146921c4bd66998de2c9530a5870f
VladaMamutova <vlada.mamutova@gmail.com>
2019-12-08 21:18:41 +0300

    feat: update MainWindow layout, add Popup Box for email settings

--- CryptoMailClient/Views/MainWindow.xaml.cs
+++ CryptoMailClient/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using CryptoMailClient.Models;
 
 namespace CryptoMailClient.Views
@@ -11,6 +13,30 @@ namespace CryptoMailClient.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ButtonExpand_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                ((Button)sender).ToolTip = "Развернуть";
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                ((Button)sender).ToolTip = "Свернуть";
+            }
+        }
+
+        private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
